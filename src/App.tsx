7e411b0e72f0a35a00a1f0c2d04c76859397c390
Tiretumbes <tiretumbes4@gmail.com>
2026-09-04 10:16:45 -0300

    PROJETO COMMITADO

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+return (
+  <>
+
+  <div className="App">
+  
+
+  <nav>Barra de navegação</nav>
+
+  <div className="btn">
+    <button>
+      Pesquisar
+    </button>
+  </div>
+
+  
+  </div>
+
+  
+  </>
+)
+}
+export default App
